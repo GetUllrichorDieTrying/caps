47e6212289e20c343362c5cbc40725c0ed6e4809
DYLAN ULLRICH <DYLAN@GETULLRICHORDIETRYING.COM>
2022-06-28 14:40:47 -0700

    working

---
+++ vendor/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const eventPool = require('../eventPool');
+const order = require('../chance');
+
+// console.log(order);
+eventPool.on('DELIVERED', delivered);
+
+function delivered(payload) {
+  console.log('VENDOR: THANK YOU FOR DELIVERING OUR ORDER:', payload.orderID);
+}
+
+setInterval(() => {
+  eventPool.emit('PICKUP', order);
+}, 3000);
+
+module.exports = delivered;
